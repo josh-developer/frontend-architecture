9eea23b07516ffa55e48016930bb32e49dbeeb6b
Umar <umar_sadullayev@epam.com>
2025-07-03 21:35:01 +0500

    Add react host

---
+++ micro-frontend/module-federation/instructions.md
@@ -0,0 +1,4 @@
+1. Create HOST app
+npx create-react-app react-host
+
+2. 
---
+++ micro-frontend/module-federation/react-host/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <header style={{ backgroundColor: 'lightgray', padding: '20px' }}>
+        <h1>React Host App</h1>           
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
